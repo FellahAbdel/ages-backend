36e48e795db1db300eaefecc53a28425a8445f41
FellahAbdel <abdoulazizdiallofouta@gmail.com>
2025-01-22 22:32:12 +0100

    Frist commit

---
+++ src/main/java/fr/ages/projet_ages/ProjetAgesApplication.java
@@ -0,0 +1,13 @@
+package fr.ages.projet_ages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetAgesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetAgesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/ages/projet_ages/ProjetAgesApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ages.projet_ages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetAgesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
